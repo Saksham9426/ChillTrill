81635fb3e7ab673cdbc053e8a4e5565c3a5dfc44
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-07-08 18:43:15 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -34,66 +34,45 @@ def crop(clahe_image, face):
         facedict["face%s" %(len(facedict)+1)]=faceslice
     return faceslice
 
-def grab_face():
-    ret, frame=video_capture.read()
-    #cv2.imshow("Video", frame)
-    cv2.imwrite('WD_INNOVATIVE/test.jpg', frame)
-    cv2.imwrite("WD_INNOVATIVE/main%s.jpg" %count, frame)
-    gray=cv2.imread('WD_INNOVATIVE/test.jpg',0)
-    #gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    clahe=cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    clahe_image=clahe.apply(gray)
-    return clahe_image
+def detect_face(frame):
+	#cv2.imshow("Video", frame)
+	cv2.imwrite('test.jpg', frame)
+	cv2.imwrite("main%s.jpg" %count, frame)
+	gray=cv2.imread('test.jpg',0)
+	#gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	clahe=cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+	clahe_image=clahe.apply(gray)
+	face=facecascade.detectMultiScale(clahe_image, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
+	if len(face)>=1:
+		faceslice=crop(clahe_image, face)
+	#return faceslice
+	else:
+		print("No/Multiple faces detected!!, passing over the frame")
 
-def detect_face():
-    clahe_image=grab_face()
-    face=facecascade.detectMultiScale(clahe_image, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
-    if len(face)>=1:
-        faceslice=crop(clahe_image, face)
-        #return faceslice
-    else:
-        print("No/Multiple faces detected!!, passing over the frame")
-
-def save_face(emotion):
-    print("\n\nLook "+emotion+" untill the timer expires and keep the same emotion for some time.")
-    #winsound.Beep(frequency, duration)
-    print('\a')
-    
-    
-    for i in range(0, 5):
-        print(5-i)
-        time.sleep(1)
-    
-    while len(facedict.keys())<16:
-        detect_face()
-
-    for i in facedict.keys():
-        path, dirs, files = next(os.walk("dataset/%s" %emotion))
-        file_count = len(files)+1
-        cv2.imwrite("dataset/%s/%s.jpg" %(emotion, (file_count)), facedict[i])
-    facedict.clear()
 def identify_emotions():
   prediction=[]
   confidence=[]
 
   for i in facedict.keys():
       pred, conf=fishface.predict(facedict[i])
-      cv2.imwrite("WD_INNOVATIVE/%s.jpg" %i, facedict[i])
+      cv2.imwrite("%s.jpg" %i, facedict[i])
       prediction.append(pred)
       confidence.append(conf)
   output=emotions[max(set(prediction), key=prediction.count)]    
   print("You seem to be %s" %output) 
   facedict.clear()
   return output;
-def getEmotion():
-    count=0
-    while True:
-        count=count+1
-        detect_face()
-        if count==10:
-            fishface.read("WD_INNOVATIVE/model.xml")
-            return identify_emotions()
-            break
+def getEmotion(frame):
+	frm = frame.to_ndarray(format="bgr24")
+		frm = cv2.flip(frm, 1)
+	count=0
+	while True:
+	count=count+1
+	detect_face(frm)
+	if count==10:
+	    fishface.read("model.xml")
+	    return identify_emotions()
+	    break
 webrtc_streamer(key="key", desired_playing_state=True,
 				video_processor_factory=getEmotion,
 				media_stream_constraints={"video": True, "audio": False},rtc_configuration={
