6f3dd9eaf5c89373ad363837398645568b6b1be0
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-07-09 13:13:42 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -65,22 +65,22 @@ def identify_emotions():
   return output;
 class VideoProcessor:
 	def transform(self, frame):
-	img = frame.to_ndarray(format="bgr24")
-	
-	#image gray
-	img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	faces=facecascade.detectMultiScale(clahe_image, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
-	for (x, y, w, h) in faces:
-		faceslice=clahe_image[y:y+h, x:x+w]
-		faceslice=cv2.resize(img_gray, (350, 350))
-		if np.sum([faceslice]) != 0:
-			pred, conf=fishface.predict(faceslice)
-			finalout = emotion[pred]
-			output = str(finalout)
-		label_position = (x, y)
-		cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-	
-	return img
+		img = frame.to_ndarray(format="bgr24")
+		
+		#image gray
+		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		faces=facecascade.detectMultiScale(clahe_image, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
+		for (x, y, w, h) in faces:
+			faceslice=clahe_image[y:y+h, x:x+w]
+			faceslice=cv2.resize(img_gray, (350, 350))
+			if np.sum([faceslice]) != 0:
+				pred, conf=fishface.predict(faceslice)
+				finalout = emotion[pred]
+				output = str(finalout)
+			label_position = (x, y)
+			cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+		
+		return img
 
 webrtc_streamer(key="key", desired_playing_state=True,
 				video_processor_factory=VideoProcessor,
