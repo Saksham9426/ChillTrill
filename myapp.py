46b5a927e992b75910ac1ddf2f77a4d207e3f47a
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-07-09 14:31:57 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -17,39 +17,50 @@ auth_token = config.TWILIO_AUTH_TOKEN
 client = Client(account_sid, auth_token)
 token = client.tokens.create()
 
-# load model
-fishface = cv2.face.FisherFaceRecognizer_create()
-font = cv2.FONT_HERSHEY_SIMPLEX
+# Define the emotions.
+emotion_labels = ['Angry','Disgust','Fear','Happy','Neutral', 'Sad', 'Surprise']
+
+# Load model.
+classifier =load_model('model_78.h5')
+
+# load weights into new model
+classifier.load_weights("model_weights_78.h5")
+
+# Load face using OpenCV
 try:
-	fishface.read(os.path.join(path,"model.xml"))
-except:
-	st.write("No trained model found... --update will create one.")
-try:
-	facecascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-except:
-	st.write("classifier not loaded")
-class VideoProcessor():
-	def transform(self, frame):
-		img = frame.to_ndarray(format="bgr24")
-		img = cv2.flip(img,1)
-		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		faces = facecascade.detectMultiScale(image=img_gray, scaleFactor=1.3, minNeighbors=5)
-		for (x, y, w, h) in faces:
-			cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-			roi_gray = img_gray[y:y + h, x:x + w]
-			roi_gray = cv2.resize(roi_gray, (350,350))
-			if np.sum([roi_gray]) != 0:
-				pred, conf=fishface.predict(roi_gray)
-				st.write(pred)
-				finalout = emotion[pred]
-				output = str(finalout)
-			label_position = (x, y)
-			cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-		
-		return img
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(0, 255, 255), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_labels[maxindex]
+                output = str(finalout)
+            label_position = (x, y-10)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        return img
+
 
 webrtc_streamer(key="key", desired_playing_state=True,
-				video_processor_factory=VideoProcessor,
+				video_processor_factory=VideoTransformer,
 				media_stream_constraints={"video": True, "audio": False},rtc_configuration={
       "iceServers": token.ice_servers
   })
