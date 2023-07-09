0650be4f109fae3f11276788aa683d8d35eb11e2
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-07-09 11:26:56 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -75,7 +75,7 @@ class VideoProcessor:
 		return av.VideoFrame.from_ndarray(frm, format='bgr24')
 
 webrtc_streamer(key="key", desired_playing_state=True,
-				video_processor_factory=EmotionProcessor,
+				video_processor_factory=VideoProcessor,
 				media_stream_constraints={"video": True, "audio": False},rtc_configuration={
       "iceServers": token.ice_servers
   })
