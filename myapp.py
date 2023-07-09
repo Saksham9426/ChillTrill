9706dfe08f2acdd0d664d168c74b9839941f1529
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-07-09 13:54:38 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -23,26 +23,25 @@ try:
 	fishface.load('model.xml')
 	facecascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 except:
-    st.write("No trained model found... --update will create one.")
+	st.write("No trained model found... --update will create one.")
 
 class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        #image gray
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        face=facecascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (350,350))
-            if np.sum([roi_gray]) != 0:
-		    pred, conf=fishface.predict(roi_gray)
-		    finalout = emotion[pred]
-		    output = str(finalout)
+	def transform(self, frame):
+		img = frame.to_ndarray(format="bgr24")
+		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		face=facecascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=15, minSize=(10, 10), flags=cv2.CASCADE_SCALE_IMAGE)
+		for (x, y, w, h) in faces:
+			cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
+			roi_gray = img_gray[y:y + h, x:x + w]
+			roi_gray = cv2.resize(roi_gray, (350,350))
+			if np.sum([roi_gray]) != 0:
+				pred, conf=fishface.predict(roi_gray)
+				finalout = emotion[pred]
+				output = str(finalout)
 			label_position = (x, y)
 			cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        return img
+		
+		return img
 
 webrtc_streamer(key="key", desired_playing_state=True,
 				video_transformer_factory=VideoTransformer,
