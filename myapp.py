26fd5bf191e65a0dc617a04621c1e8d79076df5a
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-07-10 13:08:36 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -37,36 +37,36 @@ classifier.load_weights("model_weights_78.h5")
 
 # Load face using OpenCV
 try:
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+	face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 except Exception:
-    st.write("Error loading cascade classifiers")
+	st.write("Error loading cascade classifiers")
 
 class VideoTransformer(VideoTransformerBase):
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        #image gray
-        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(
-            image=img_gray, scaleFactor=1.3, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img=img, pt1=(x, y), pt2=(
-                x + w, y + h), color=(0, 255, 255), thickness=2)
-            roi_gray = img_gray[y:y + h, x:x + w]
-            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
-            if np.sum([roi_gray]) != 0:
-                roi = roi_gray.astype('float') / 255.0
-                roi = img_to_array(roi)
-                roi = np.expand_dims(roi, axis=0)
-                prediction = classifier.predict(roi)[0]
-                maxindex = int(np.argmax(prediction))
-                finalout = emotion_labels[maxindex]
-                output = str(finalout)
-		    np.save("emotion.npy", np.array([output]))
-            label_position = (x, y-10)
-            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-        return img
+	def transform(self, frame):
+		img = frame.to_ndarray(format="bgr24")
+		
+		#image gray
+		img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		faces = face_cascade.detectMultiScale(
+		    image=img_gray, scaleFactor=1.3, minNeighbors=5)
+		for (x, y, w, h) in faces:
+			cv2.rectangle(img=img, pt1=(x, y), pt2=(
+			x + w, y + h), color=(0, 255, 255), thickness=2)
+			roi_gray = img_gray[y:y + h, x:x + w]
+			roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+			if np.sum([roi_gray]) != 0:
+				roi = roi_gray.astype('float') / 255.0
+				roi = img_to_array(roi)
+				roi = np.expand_dims(roi, axis=0)
+				prediction = classifier.predict(roi)[0]
+				maxindex = int(np.argmax(prediction))
+				finalout = emotion_labels[maxindex]
+				output = str(finalout)
+			np.save("emotion.npy", np.array([output]))
+			label_position = (x, y-10)
+			cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+	
+		return img
 
 
 webrtc_streamer(key="key", desired_playing_state=True,
